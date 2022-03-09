b2decdf6a5517f4df93eff2ffd69880781df6dd6
Yustynn Panicker <yustynn@gmail.com>
2022-03-09 16:18:31 +0800

    Remove all PDF-related things, add d3 and katex

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-import { x } from './test'
-import pdf from 'pdfjs-dist'
-import * as _ from 'lodash'
-
-console.log('pdf', pdf)
-console.log('lodash', _)
-console.log(x)
+console.log('Sup world?!')
