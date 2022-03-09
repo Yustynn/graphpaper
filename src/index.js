08bdb8239d5118e80adbfcb223ea3491b16243b5
Yustynn Panicker <yustynn@gmail.com>
2022-03-09 16:34:31 +0800

    Add graph data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Sup world?!')
+import yaml from 'yaml'
+const GRAPH_DATA_PATH = './data/graph.yaml'
+async function main() {
+  const data = await fetch(GRAPH_DATA_PATH)
+    .then(r => r.text())
+    .then(yaml.parse)
+
+  console.log(data)
+}
+
+main()
